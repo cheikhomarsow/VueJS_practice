b03028e4bfc13aa2bd16ffe8acb87b9bb78c6cef
cheikhomarsow <cheikh-omar.sow@supinternet.fr>
2018-05-23 06:28:44 +0200

    Filter - search

---
+++ tutorial/ib/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <show-blogs></show-blogs>
+  </div>
+</template>
+
+<script>
+
+import addBlog from './components/addBlog.vue'
+import showBlogs from './components/showBlogs.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+
+  },
+  components: {
+    'add-blog': addBlog,
+    'show-blogs': showBlogs
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+}
+</style>
---
+++ tutorial/ib/src/main.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource);
+
+Vue.config.productionTip = false
+
+//Filters
+Vue.filter('to-uppercase', function(value){
+  return value.toUpperCase();
+});
+
+Vue.filter('snippet', function(value){
+  return value.slice(0,100) + '...';
+});
+
+//Custom directives
+Vue.directive('rainbow', {
+  bind(el,binding,vnode){
+    el.style.color = "#" + Math.random().toString().slice(2,8);
+  }
+});
+
+Vue.directive('theme', {
+  bind(el, binding, vnode){
+    if(binding.value == 'wide'){
+      el.style.maxWidth = "1200px";
+    }else if(binding.value == 'narrow'){
+        el.style.maxWidth = "450px";
+    }
+    if(binding.arg == 'column'){
+      el.style.background = '#ddd';
+      el.style.padding = '20px';
+    }
+  }
+});
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
